cde2cb727fb943dd228df19332ee5766cb1a883f
Osama Almelhem <osamaalmelhem2002@gmail.com>
2024-06-20 15:18:20 +0300

    third version - added animations

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -26,21 +26,35 @@ class MealDetailsScreen extends ConsumerWidget {
               ScaffoldMessenger.of(context).clearSnackBars();
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
-                  content: Text(wasAdded ? 'Added to favorites.':'Removed from favorites.'),
+                  content: Text(wasAdded
+                      ? 'Added to favorites.'
+                      : 'Removed from favorites.'),
                 ),
               );
             },
-            icon: Icon(isFavorite ?  Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) => RotationTransition(
+                      turns: Tween(begin: 0.8, end: 0.9).animate(animation),
+                      child: child,
+                    ),
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                )),
           )
         ]),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(height: 14),
               Text(
